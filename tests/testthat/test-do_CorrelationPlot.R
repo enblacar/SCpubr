b30c448c801dbd31da145be303071d9e0680f3f6
enblacar <e.blancocarmona@dkfz-heidelberg.de>
2022-06-19 23:11:54 +0200

    Added unit tests for do_CorrelationPlot.R

---
+++ tests/testthat/test-do_CorrelationPlot.R
@@ -0,0 +1,41 @@
+sample <- use_dataset()
+sample$orig.ident <- ifelse(sample$seurat_clusters %in% c("1", "2"), "A", "B")
+
+genes <- list("A" = Seurat::VariableFeatures(sample)[1:5],
+              "B" = Seurat::VariableFeatures(sample)[6:10],
+              "C" = Seurat::VariableFeatures(sample)[11:15])
+
+testthat::test_that("do_CorrelationPlot: PASS - normal", {
+  p <- SCpubr::do_CorrelationPlot(sample = sample)
+  testthat::expect_true("HeatmapList" %in% class(p))
+})
+
+testthat::test_that("do_CorrelationPlot: PASS - group.by", {
+  p <- SCpubr::do_CorrelationPlot(sample = sample,
+                                  group.by = "orig.ident")
+  testthat::expect_true("HeatmapList" %in% class(p))
+})
+
+testthat::test_that("do_CorrelationPlot: PASS - group.by - rotate axis labels", {
+  p <- SCpubr::do_CorrelationPlot(sample = sample,
+                                  group.by = "orig.ident",
+                                  column_names_rot = 0)
+  testthat::expect_true("HeatmapList" %in% class(p))
+})
+
+testthat::test_that("do_CorrelationPlot: PASS - group.by - cell size", {
+  p <- SCpubr::do_CorrelationPlot(sample = sample,
+                                  group.by = "orig.ident",
+                                  column_names_rot = 0,
+                                  cell_size = 7)
+  testthat::expect_true("HeatmapList" %in% class(p))
+})
+
+testthat::test_that("do_CorrelationPlot: PASS - group.by - colors", {
+  p <- SCpubr::do_CorrelationPlot(sample = sample,
+                                  group.by = "orig.ident",
+                                  column_names_rot = 0,
+                                  cell_size = 7,
+                                  colors.use = c("#e9d8a6", "#9b2226"))
+  testthat::expect_true("HeatmapList" %in% class(p))
+})
