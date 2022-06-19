6b8d26ca783dd25e816f747c6678b8e9a5eaa43e
enblacar <e.blancocarmona@dkfz-heidelberg.de>
2022-06-19 23:58:55 +0200

    changes to unit tests

--- tests/testthat/test-save_Plot.R
+++ tests/testthat/test-save_Plot.R
@@ -1,53 +1,57 @@
 sample <- use_dataset()
 p <- do_DimPlot(sample)
-
+figure_path <- "./"
 testthat::test_that("save_Plot: PASS - all", {
   testthat::expect_silent(save_Plot(plot = p,
-                                    figure_path = "/b06x-isilon/b06x-g/G703/eblanco/projects/SCpubr/tests/temp",
+                                    figure_path = figure_path,
                                     file_name = "test",
                                     output_format = "all"))
 })
 
 testthat::test_that("save_Plot: PASS - publication", {
   testthat::expect_silent(save_Plot(plot = p,
-                                    figure_path = "/b06x-isilon/b06x-g/G703/eblanco/projects/SCpubr/tests/temp",
+                                    figure_path = figure_path,
                                     file_name = "test",
                                     output_format = "publication"))
 })
 
 testthat::test_that("save_Plot: PASS - jpeg", {
   testthat::expect_silent(save_Plot(plot = p,
-                                    figure_path = "/b06x-isilon/b06x-g/G703/eblanco/projects/SCpubr/tests/temp",
+                                    figure_path = figure_path,
                                     file_name = "test",
                                     output_format = "jpeg"))
 })
 
 testthat::test_that("save_Plot: PASS - png", {
   testthat::expect_silent(save_Plot(plot = p,
-                                    figure_path = "/b06x-isilon/b06x-g/G703/eblanco/projects/SCpubr/tests/temp",
+                                    figure_path = figure_path,
                                     file_name = "test",
                                     output_format = "png"))
 })
 
 testthat::test_that("save_Plot: PASS - pdf", {
   testthat::expect_silent(save_Plot(plot = p,
-                                    figure_path = "/b06x-isilon/b06x-g/G703/eblanco/projects/SCpubr/tests/temp",
+                                    figure_path = figure_path,
                                     file_name = "test",
                                     output_format = "pdf"))
 })
 
 testthat::test_that("save_Plot: PASS - tiff", {
   testthat::expect_silent(save_Plot(plot = p,
-                                    figure_path = "/b06x-isilon/b06x-g/G703/eblanco/projects/SCpubr/tests/temp",
+                                    figure_path = figure_path,
                                     file_name = "test",
                                     output_format = "tiff"))
 })
 
 testthat::test_that("save_Plot: PASS - svg", {
   testthat::expect_silent(save_Plot(plot = p,
-                                    figure_path = "/b06x-isilon/b06x-g/G703/eblanco/projects/SCpubr/tests/temp",
+                                    figure_path = figure_path,
                                     file_name = "test",
                                     output_format = "svg"))
 })
 
-unlink("/b06x-isilon/b06x-g/G703/eblanco/projects/SCpubr/tests/temp/", recursive = T)
+unlink(paste0(figure_path, "test.svg"))
+unlink(paste0(figure_path, "test.jpeg"))
+unlink(paste0(figure_path, "test.pdf"))
+unlink(paste0(figure_path, "test.tiff"))
+unlink(paste0(figure_path, "test.png"))
