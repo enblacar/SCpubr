1caa1b089fabaaab72b3293df2f713aa62145a88
enblacar <e.blancocarmona@dkfz-heidelberg.de>
2022-08-13 18:10:33 +0200

    Fixing bugs from last commits. - v2

--- R/do_NebulosaPlot.R
+++ R/do_NebulosaPlot.R
@@ -208,16 +208,30 @@ do_NebulosaPlot <- function(sample,
       # Set size of dots.
       p[[plot_num]]$layers[[1]]$aes_params$size <- pt.size
 
-      p[[plot_num]] <- modify_continuous_legend(p = p[[plot_num]],
-                                                legend.aes = "color",
-                                                legend.type = legend.type,
-                                                legend.position = legend.position,
-                                                legend.length = legend.length,
-                                                legend.width = legend.width,
-                                                legend.framecolor = legend.framecolor,
-                                                legend.tickcolor = legend.tickcolor,
-                                                legend.framewidth = legend.framewidth,
-                                                legend.tickwidth = legend.tickwidth)
+      if (num_plots == 1){
+        p <- modify_continuous_legend(p = p,
+                                      legend.aes = "color",
+                                      legend.type = legend.type,
+                                      legend.position = legend.position,
+                                      legend.length = legend.length,
+                                      legend.width = legend.width,
+                                      legend.framecolor = legend.framecolor,
+                                      legend.tickcolor = legend.tickcolor,
+                                      legend.framewidth = legend.framewidth,
+                                      legend.tickwidth = legend.tickwidth)
+      } else {
+        p[[plot_num]] <- modify_continuous_legend(p = p[[plot_num]],
+                                                  legend.aes = "color",
+                                                  legend.type = legend.type,
+                                                  legend.position = legend.position,
+                                                  legend.length = legend.length,
+                                                  legend.width = legend.width,
+                                                  legend.framecolor = legend.framecolor,
+                                                  legend.tickcolor = legend.tickcolor,
+                                                  legend.framewidth = legend.framewidth,
+                                                  legend.tickwidth = legend.tickwidth)
+      }
+
     }
 
     # For embeddings that are umap of tsne, we remove all axes..
