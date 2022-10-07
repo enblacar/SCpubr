455e619d77f572ece8b17a02135ac63a85e14f62
enblacar <e.blancocarmona@dkfz-heidelberg.de>
2022-10-07 15:07:12 +0200

    Added more unit tests for do_ChordDiagramPlot.

--- tests/testthat/test-do_ChordDiagramPlot.R
+++ tests/testthat/test-do_ChordDiagramPlot.R
@@ -1,10 +1,61 @@
 sample <- SCpubr:::test_list$sample
 
 testthat::test_that("do_ChordDiagramPlot: PASS - default", {
+  sample$seurat_clusters_char <- as.character(sample$seurat_clusters)
+  sample$orig.ident_char <- sample$orig.ident
+  sample$orig.ident <- factor(sample$orig.ident)
   p <- SCpubr::do_ChordDiagramPlot(sample = sample,
                                    from = "seurat_clusters",
                                    to = "orig.ident")
   testthat::expect_type(p, "list")
+
+  p <- SCpubr::do_ChordDiagramPlot(sample = sample,
+                                   from = "seurat_clusters_char",
+                                   to = "orig.ident_char")
+  testthat::expect_type(p, "list")
+})
+
+
+testthat::test_that("do_ChordDiagramPlot: PASS - colors", {
+  p <- SCpubr::do_ChordDiagramPlot(sample = sample,
+                                   from = "orig.ident",
+                                   to = "seurat_clusters",
+                                   colors.from = c("Cell" = "blue"))
+  testthat::expect_type(p, "list")
+
+  p <- SCpubr::do_ChordDiagramPlot(sample = sample,
+                                   from = "seurat_clusters",
+                                   to = "orig.ident",
+                                   colors.to = c("Cell" = "blue"))
+  testthat::expect_type(p, "list")
+
+  p <- SCpubr::do_ChordDiagramPlot(sample = sample,
+                                   from = "orig.ident",
+                                   to = "seurat_clusters",
+                                   colors.from = c("Cell" = "#345211"))
+  testthat::expect_type(p, "list")
+
+  p <- SCpubr::do_ChordDiagramPlot(sample = sample,
+                                   from = "orig.ident",
+                                   to = "seurat_clusters",
+                                   colors.from = c("Cell" = "#345211FF"))
+  testthat::expect_type(p, "list")
+
+
+  p <- SCpubr::do_ChordDiagramPlot(sample = sample,
+                                   from = "orig.ident",
+                                   to = "seurat_clusters",
+                                   colors.from = c("Cell" = "#345211"),
+                                   highlight_group = "Cell")
+  testthat::expect_type(p, "list")
+
+  p <- SCpubr::do_ChordDiagramPlot(sample = sample,
+                                   from = "orig.ident",
+                                   to = "seurat_clusters",
+                                   colors.from = c("Cell" = "#345211FF"),
+                                   highlight_group = "Cell")
+  testthat::expect_type(p, "list")
+
 })
 
 testthat::test_that("do_ChordDiagramPlot: PASS - link border color", {
@@ -42,6 +93,117 @@ testthat::test_that("do_ChordDiagramPlot: FAILS", {
                                                       from = "seurat_clusters",
                                                       to = "orig.ident",
                                                       alignment = "wrong")})
+
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident",
+                                                      link.arr.type = "wrong")})
+
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident",
+                                                      highlight_group = "0",
+                                                      alpha.highlight = 120)})
+
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = NULL,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident")})
+
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = NULL,
+                                                      to = "orig.ident")})
+
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = NULL)})
+
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident",
+                                                      directional = 4)})
+
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident",
+                                                      direction.type = "wrong")})
+})
+
+
+testthat::test_that("do_ChordDiagramPlot: internal_use", {
+  liana_output <- SCpubr:::test_list$liana_output
+
+  liana_output <- liana_output %>%
+                  liana::liana_aggregate(verbose = FALSE) %>%
+                  dplyr::mutate(magnitude = .data$sca.LRscore) %>%
+                  dplyr::mutate(specificity = .data$natmi.edge_specificity) %>%
+                  dplyr::arrange(dplyr::desc(.data$specificity), dplyr::desc(.data$magnitude)) %>%
+                  tidyr::unite(c("ligand.complex", "receptor.complex"),
+                               col = "interaction",
+                               sep = " | ",
+                               remove = FALSE) %>%
+                  # Merge source and target column into one, for future filtering.
+                  tidyr::unite(c("source", "target"),
+                               col = "interacting_clusters",
+                               remove = FALSE)
+  output_copy <- liana_output %>% dplyr::filter(.data$aggregate_rank <= 0.05)
+  liana_output <- liana_output %>%
+                  # Filter based on the top X interactions of ascending sensibilities.
+                  dplyr::inner_join(y = {liana_output %>%
+                                         dplyr::distinct_at(dplyr::all_of(c("ligand.complex", "receptor.complex"))) %>%
+                                         dplyr::slice_head(n = 25)},
+                                         by = c("ligand.complex", "receptor.complex"))
+
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident",
+                                                      from_df = TRUE,
+                                                      df = 3)})
+
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident",
+                                                      from_df = TRUE,
+                                                      df = as.data.frame(liana_output))})
+
+  data <- output_copy %>%
+          dplyr::select(dplyr::all_of(c("source", "target"))) %>%
+          dplyr::group_by(.data$target, .data$source) %>%
+          dplyr::summarise(value = dplyr::n()) %>%
+          dplyr::rename("from" = .data[["source"]],
+                        "to" = .data[["target"]]) %>%
+          dplyr::select(dplyr::all_of(c("from", "to", "value")))
+  data.test <- data
+  colnames(data.test) <- c("A", "B", "C")
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident",
+                                                      from_df = TRUE,
+                                                      df = data.test)})
+
+  data.test <- data
+  data.test$from <- 3
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident",
+                                                      from_df = TRUE,
+                                                      df = data.test)})
+
+  data.test <- data
+  data.test$to <- 3
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident",
+                                                      from_df = TRUE,
+                                                      df = data.test)})
+
+  data.test <- data
+  data.test$value <- "B"
+  testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
+                                                      from = "seurat_clusters",
+                                                      to = "orig.ident",
+                                                      from_df = TRUE,
+                                                      df = data.test)})
+
 
   testthat::expect_error({SCpubr::do_ChordDiagramPlot(sample = sample,
                                                       from = "seurat_clusters",
--- tests/testthat/test-do_VolcanoPlot.R
+++ tests/testthat/test-do_VolcanoPlot.R
@@ -1,7 +1,23 @@
 sample <- SCpubr:::test_list$sample
 de_genes <- SCpubr:::test_list$de_genes
+`%>%` <- magrittr::`%>%`
 
 testthat::test_that("do_VolcanoPlot: PASS - default", {
+  p <- SCpubr::do_VolcanoPlot(sample = sample,
+                              de_genes = de_genes)
+  testthat::expect_type(p, "list")
+
+  de_genes[1, "p_val_adj"] <- 1
+  de_genes[2, "avg_log2FC"] <- 0.001
+
+  p <- SCpubr::do_VolcanoPlot(sample = sample,
+                              de_genes = de_genes)
+  testthat::expect_type(p, "list")
+
+  de_genes <- de_genes %>%
+              dplyr::distinct(.data$gene, .keep_all = TRUE) %>%
+              tibble::column_to_rownames(var = "gene")
+
   p <- SCpubr::do_VolcanoPlot(sample = sample,
                               de_genes = de_genes)
   testthat::expect_type(p, "list")
