d165dff0d3832e896ce8447cb155fc9a89231038
enblacar <e.blancocarmona@dkfz-heidelberg.de>
2022-10-07 13:45:31 +0200

    Added more  unit tests to sankey plots.

--- tests/testthat/test-do_SankeyPlot.R
+++ tests/testthat/test-do_SankeyPlot.R
@@ -2,7 +2,7 @@ sample <- SCpubr:::test_list$sample
 
 testthat::test_that("do_SankeyPlot: FAIL - no dplyr", {
   testthat::expect_error({SCpubr::do_SankeyPlot(sample = sample,
-                                                first_group = "seurat_cluters",
+                                                first_group = "seurat_clusters",
                                                 last_group = "orig.ident")})
 })
 suppressMessages(library(dplyr))
@@ -20,6 +20,15 @@ testthat::test_that("do_SankeyPlot: PASS - middle_groups", {
                              first_group = "seurat_clusters",
                              last_group = "orig.ident",
                              middle_group = "middle_group")
+  testthat::expect_type(p, "list")
+})
+
+testthat::test_that("do_SankeyPlot: PASS - hjust", {
+  sample$middle_group <- sample$seurat_clusters
+  p <- SCpubr::do_SankeyPlot(sample = sample,
+                             first_group = "seurat_clusters",
+                             last_group = "orig.ident",
+                             hjust = 0.5)
   testthat::expect_type(p, "list")
 })
 
@@ -60,31 +69,31 @@ testthat::test_that("do_SankeyPlot: FAILS", {
                                                 first_group = "orig.ident")})
 
   testthat::expect_error({SCpubr::do_SankeyPlot(sample = sample,
-                                                first_group = "seurat_cluters",
+                                                first_group = "seurat_clusters",
                                                 last_group = "orig.ident",
                                                 colors.first = c("A" = "red"))})
   testthat::expect_error({SCpubr::do_SankeyPlot(sample = sample,
-                                                first_group = "seurat_cluters",
+                                                first_group = "seurat_clusters",
                                                 last_group = "orig.ident",
                                                 colors.first = c("0" = "red"))})
 
   testthat::expect_error({SCpubr::do_SankeyPlot(sample = sample,
-                                                first_group = "seurat_cluters",
+                                                first_group = "seurat_clusters",
                                                 middle_groups = "middle_group",
                                                 last_group = "orig.ident",
                                                 colors.middle = c("A" = "red"))})
   testthat::expect_error({SCpubr::do_SankeyPlot(sample = sample,
-                                                first_group = "seurat_cluters",
+                                                first_group = "seurat_clusters",
                                                 middle_groups = "middle_group",
                                                 last_group = "orig.ident",
                                                 colors.middle = c("0" = "red"))})
   sample$orig.ident <- ifelse(sample$seurat_clusters == "0", "C", "B")
   testthat::expect_error({SCpubr::do_SankeyPlot(sample = sample,
-                                                first_group = "seurat_cluters",
+                                                first_group = "seurat_clusters",
                                                 last_group = "orig.ident",
                                                 colors.last = c("A" = "red"))})
   testthat::expect_error({SCpubr::do_SankeyPlot(sample = sample,
-                                                first_group = "seurat_cluters",
+                                                first_group = "seurat_clusters",
                                                 last_group = "orig.ident",
                                                 colors.last = c("B" = "red"))})
 })
