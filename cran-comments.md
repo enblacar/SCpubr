e6358b29ecad74a6da40830b9403c212b94deb0a
enblacar <e.blancocarmona@dkfz-heidelberg.de>
2023-01-12 12:06:00 +0100

    Another reiteration of CRAN checks for the new version.

--- cran-comments.md
+++ cran-comments.md
@@ -1,10 +1,13 @@
+# Resubmission version 1.1.1
+Fixed the conditional use of AnnotationDbi package. If it still does not work properly, I would really appreciate some further input on how to solve it. Thanks!
+
 # Submission version 1.1.1
 Hotfixes to unit tests to comply with CRAN internal testings. It was asked to be corrected before 25-01-2023.
 
-## R CMD check results
+## `devtools` R CMD check results
 There were no ERRORs, WARNINGs or NOTEs. 
 
-## R CMD check results `_R_CHECK_DEPENDS_ONLY_` = TRUE
+## `devtools` R CMD check results `_R_CHECK_DEPENDS_ONLY_` = TRUE
 There were no ERRORs, WARNINGs or NOTEs.
 
 ## Downstream dependencies
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -3,9 +3,7 @@ if (requireNamespace("Seurat", quietly = TRUE)) {
  suppressMessages(library("Seurat"))
 }
 
-if (requireNamespace("org.Hs.eg.db", quietly = TRUE)) {
-  suppressMessages(library("org.Hs.eg.db"))
-}
+
 
 if (requireNamespace("magrittr", quietly = TRUE)) {
  suppressMessages(library("magrittr"))
@@ -17,6 +15,8 @@ if (requireNamespace("dplyr", quietly = TRUE)) {
                                    "avg_diff" = "avg_log2FC")
 }
 
+
+
 sample <- readRDS(system.file("extdata/seurat_dataset_example.rds", package = "SCpubr"))
 metacell_mapping <- readRDS(system.file("extdata/metacell_mapping_example.rds", package = "SCpubr"))
 infercnv_object <- readRDS(system.file("extdata/infercnv_object_example.rds", package = "SCpubr"))
@@ -25,7 +25,6 @@ human_chr_locations <- SCpubr::human_chr_locations
 progeny_activities <- readRDS(system.file("extdata/progeny_activities_example.rds", package = "SCpubr"))
 dorothea_activities <- readRDS(system.file("extdata/dorothea_activities_example.rds", package = "SCpubr"))
 enriched_terms <- readRDS(system.file("extdata/enriched_terms_example.rds", package = "SCpubr"))
-org.db <- AnnotationDbi::loadDb(system.file("./extdata/org.Hs.eg.sqlite", package = "org.Hs.eg.db"))
 
 
 # Get packages.
@@ -59,24 +58,34 @@ for (func in names(dependencies)){
   dep_check[[func]] <- value
 }
 
+if (isFALSE(dep_check[["do_GroupedGOTermPlot"]]) | isFALSE(dep_check[["do_FunctionalAnnotationPlot"]])){
+  if (requireNamespace("org.Hs.eg.db", quietly = TRUE)) {
+    suppressMessages(library("org.Hs.eg.db"))
+  }
+
+  if (requireNamespace("AnnotationDbi", quietly = TRUE)) {
+    suppressMessages(library("AnnotationDbi"))
+    org.db <- AnnotationDbi::loadDb(system.file("./extdata/org.Hs.eg.sqlite", package = "org.Hs.eg.db"))
+  }
+}
 
 # Remove this for publication in CRAN.
-if (isFALSE(dep_check[["do_LigandReceptorPlot"]])){
-  liana_output <- readRDS(system.file("extdata/liana_output_example.rds", package = "SCpubr"))
-}
-
-if (isFALSE(dep_check[["do_DimPlot"]]) &
-    isFALSE(dep_check[["do_CorrelationPlot"]]) &
-    isFALSE(dep_check[["do_ChordDiagramPlot"]]) &
-    isTRUE(requireNamespace(pkg, quietly = TRUE)) &
-    isFALSE(dep_check[["save_Plot"]])){
-  p <- SCpubr::do_DimPlot(sample)
-  p.heatmap <- SCpubr::do_CorrelationPlot(sample)
-  data <- p.heatmap@ht_list$`Pearson coef.`@matrix
-  p.pheatmap <- pheatmap::pheatmap(data)
-  p.chord <- SCpubr::do_ChordDiagramPlot(sample = sample, from = "seurat_clusters", to = "orig.ident")
-  figure_path <- getwd()
-}
+#if (isFALSE(dep_check[["do_LigandReceptorPlot"]])){
+#  liana_output <- readRDS(system.file("extdata/liana_output_example.rds", package = "SCpubr"))
+#}
+#
+#if (isFALSE(dep_check[["do_DimPlot"]]) &
+#    isFALSE(dep_check[["do_CorrelationPlot"]]) &
+#    isFALSE(dep_check[["do_ChordDiagramPlot"]]) &
+#    isTRUE(requireNamespace(pkg, quietly = TRUE)) &
+#    isFALSE(dep_check[["save_Plot"]])){
+#  p <- SCpubr::do_DimPlot(sample)
+#  p.heatmap <- SCpubr::do_CorrelationPlot(sample)
+#  data <- p.heatmap@ht_list$`Pearson coef.`@matrix
+#  p.pheatmap <- pheatmap::pheatmap(data)
+#  p.chord <- SCpubr::do_ChordDiagramPlot(sample = sample, from = "seurat_clusters", to = "orig.ident")
+#  figure_path <- getwd()
+#}
 
 
 #monocle_sample <- sample
